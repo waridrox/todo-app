5565d64bd25a9de1186dd631596833ef66b71ae7
waridrox <mohdwarid4@gmail.com>
2021-10-28 13:05:31 +0530

    es lint fix

--- src/App.vue
+++ src/App.vue
@@ -13,9 +13,9 @@
 </template>
 
 <script>
-import Todos from '@/components/Todos.vue';
-import AddTodo from '@/components/AddTodo.vue';
-import FilterTodos from '@/components/FilterTodos.vue'
+import Todos from "@/components/Todos.vue";
+import AddTodo from "@/components/AddTodo.vue";
+import FilterTodos from "@/components/FilterTodos.vue";
 export default {
   name: "App",
   components: {
@@ -28,9 +28,9 @@ export default {
 
 <style>
 body {
-  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
   line-height: 1.6;
-  background: #e8f7f0
+  background: #e8f7f0;
 }
 .container {
   max-width: 1100px;
